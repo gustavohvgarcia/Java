38b7ad0b415ee207204eb30e5c261f2e5a6a3c8b
gustavohvgarcia <gustavo.henrique420@uel.br>
2021-07-22 19:31:51 -0300

    Projeto referente ao curso de IDES oferecido pela DIO

---
+++ Dominando-IDES-bootcamp-DIO/projetojeto/src/projetojeto/Principal.java
@@ -0,0 +1,15 @@
+package projetojeto;
+
+import br.projetojeto.Gato;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		//eu sei que a ordem dos parametros está errada. 
+		//Um gato chamado preto da cor José me soou engraçado.
+		Gato gato = new Gato("Preto", "Jose", 34);
+
+	}
+
+}
